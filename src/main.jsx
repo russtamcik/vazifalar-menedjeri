66e2ffcba6955c851135e3403e244e5888a60c02
russtamcik <abbasovr455@gmail.com>
2023-11-03 17:50:24 +0500

    change

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-import Task from "./App.jsx";
+import App from "./App";
 
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <Task />
+    <App />
   </React.StrictMode>
 );
